bd67c2ae7639932d8a1372c113d68cc184266f06
anuj <anuj@anujlaptop.server.hwm>
2015-02-07 19:29:12 +0530

    Added Routing in Contacts

---
+++ contacts/contactControllerModule.js
@@ -0,0 +1,26 @@
+var contactControllerModule=angular.module('crmApp.contactControllerModule',['crmApp.contactServiceModule']);
+contactControllerModule.controller('contactListController',function($scope,contactService)
+		{
+         init();
+         function init()
+         {
+        	 $scope.contacts=contactService.getContacts();
+        	 $scope.hide=false;
+         }
+	
+		}
+		
+);
+
+contactControllerModule.controller('viewContactController', function($scope,$routeParams,contactService){
+    var contactId;
+    $scope.contact={};
+    init();
+    function init(){
+         contactId=($routeParams.contactId);    
+         $scope.contact=contactService.getContact(contactId);   
+        
+  }
+     
+    
+});
