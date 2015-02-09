4377cc87f7ccd7197c494da55e6a43579d568744
Aman29 <29aman92@gmail.com>
2015-02-09 18:33:49 +0530

    ...

--- opportunity/opportunityServiceModule.js
+++ opportunity/opportunityServiceModule.js
@@ -17,8 +17,8 @@ this.getOpportunities=function(){
    };
 	
 var opportunities=[
-{id:1, leadName:'Amit', responsiblePerson:'Gadaley', email:'agadaley@gmail.com', phoneNo:9584593069,companyType:'hotwax systems',industry:'engineer',annualIncome: 4000,currency: 'INR',comment: 'Comment just for testing purpose amit'},
-{id:1, leadName:'Rahul', responsiblePerson:'Bhammarker', email:'rahul@gmail.com', phoneNo:111111111,companyType:'hotwax media',industry:'engineer',annualIncome: 4000,currency: 'USD',comment: 'Comment just for testing purpose by rahul'},
-{id:1, leadName:'Aman', responsiblePerson:'agrawal', email:'aman@gmail.com', phoneNo:999999999,companyType:'hotwax family',industry:'engineer',annualIncome: 4000,currency: 'USD',comment: 'Comment just for testing purpose by aman'},
+{id:1, leadName:'Amit', assignedTo:'Gadaley', nextStep:'..........', leadSource:'2',team:'hotwax systems',likely:4000,description: 'Comment just for testing purpose amit'},
+{id:2, leadName:'Saket', assignedTo:'Baghel', nextStep:'..........', leadSource:'2',team:'hotwax systems',likely:4000,description: 'Comment just for testing purpose amit'},
+{id:3, leadName:'Rahul', assignedTo:'Bhammarker', nextStep:'..........', leadSource:'2',team:'hotwax systems',likely:4000,description: 'Comment just for testing purpose amit'},
 ];
 });
