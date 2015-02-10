ab5636e981fe0c90b27bc3fb423a5d0e99e0310f
rahul <rahul@rahulbhalaptop.server.hwm>
2015-02-10 16:41:09 +0530

    F

--- opportunity/opportunityServiceModule.js
+++ opportunity/opportunityServiceModule.js
@@ -17,8 +17,8 @@ this.getOpportunities=function(){
    };
 	
 var opportunities=[
-{id:1, leadName:'Amit', assignedTo:'Gadaley', nextStep:'..........', leadSource:'2', team:'hotwax systems', likely: 4000, description: 'Comment just for testing purpose amit'},
-{id:2, leadName:'Saket', assignedTo:'Baghel', nextStep:'..........', leadSource:'2', team:'hotwax systems', likely: 4000, description: 'Comment just for testing purpose amit'},
-{id:3, leadName:'Rahul', assignedTo:'Bhammarker', nextStep:'..........', leadSource:'2', team:'hotwax systems', likely:4000, description: 'Comment just for testing purpose amit'} 
+{id:1, leadName:'Amit', assignedTo:'Anil Patel', nextStep:'Call', leadSource:'Cold call', team:'Hotwax systems', likely: 1000, description: 'Comment just for testing purpose amit'},
+{id:2, leadName:'Saket', assignedTo:'Pranay Pandey', nextStep:'Make lead', leadSource:'Conference', team:'Hotwax systems', likely: 400, description: 'Comment just for testing purpose amit'},
+{id:3, leadName:'Rahul', assignedTo:'Mridul Pathak', nextStep:'Make opportunity', leadSource:'Meeting', team:'Hotwax systems', likely:12000, description: 'Comment just for testing purpose amit'} 
 ];
 });
