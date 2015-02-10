e9fa958a25db371a30b2ae65736445f8fda6e724
saket-baghel <baghel.saket08@gmail.com>
2015-02-10 16:19:31 +0530

    ..

---
+++ dashboard/dashboardControllerModule.js
@@ -0,0 +1,25 @@
+var dashboardControllerModule=angular.module('crmApp.dashboardControllerModule');
+dashboardControllerModule.controller('dashboardController',function($scope){
+	 $scope.todos = [
+	                 {text:'Prepare Report', done:true},
+	                 {text:'Meeting @ Sayaji', done:true},
+	                 {text:'Do Homework', done:false}];
+	            
+	               $scope.addTodo = function() {
+	                 $scope.todos.push({text:$scope.todoText, done:false});
+	                 $scope.todoText = '';
+	               };
+	            
+	               $scope.remaining = function() {
+	                 var count = 0;
+	                 angular.forEach($scope.todos, function(todo) {
+	                   count += todo.done ? 0 : 1;
+	                 });
+	                 return count;
+	               };
+	            });
+
+
+
+
+
