a3c62e1517ab1d5dbc7c45dce60a3372da9f27da
Aman29 <29aman92@gmail.com>
2015-02-10 10:31:37 +0530

    ...

--- leads/leadServiceModule.js
+++ leads/leadServiceModule.js
@@ -17,8 +17,8 @@ this.getLeads=function(){
    };
 	
 var leads=[
-{id:1, leadName:'Amit', status:'In progress', sourceName:'...........',email:'gadaley@gmail.com', phoneNo:9584593069,company:'hotwax systems',addressLine1: 'vijaynagar',addressLine2: 'behind sayaji',country:'India',state:'m.p'},
-{id:2, leadName:'Rahul', status:'Assigned to', sourceName:'...........',email:'bhammarker@gmail.com', phoneNo:1111111111,company:'hotwax media',addressLine1: 'LIG',addressLine2: 'behind C21',country:'India',state:'u.p'},
-{id:3, leadName:'Saket', status:'Dead', sourceName:'...........',email:'saket@gmail.com', phoneNo:9425094250,company:'hotwax family',addressLine1: 'sneh nagar',addressLine2: 'behind Lotus',country:'India',state:'delhi'},
+{id:1, leadName:'Amit Gadaley', status:'In progress', sourceName:'call',email:'gadaley@gmail.com', phoneNo:9584593069,company:'hotwax systems',addressLine1: 'vijaynagar',addressLine2: 'behind sayaji',country:'India',state:'m.p',description:'Testing purpose from amit'},
+{id:2, leadName:'Rahul', status:'Assigned to', sourceName:'conference',email:'bhammarker@gmail.com', phoneNo:1111111111,company:'hotwax media',addressLine1: 'LIG',addressLine2: 'behind C21',country:'India',state:'u.p',description:'Testing purpose from rahul'},
+{id:3, leadName:'Saket', status:'Dead', sourceName:'meeting',email:'saket@gmail.com', phoneNo:9425094250,company:'hotwax family',addressLine1: 'sneh nagar',addressLine2: 'behind Lotus',country:'India',state:'delhi',description:'Testing purpose from saket'},
 ];
 });
