37488a8546bc5b4c0640e6db3527545e9e12c936
Aman29 <29aman92@gmail.com>
2015-02-10 09:49:47 +0530

    ...

--- leads/leadServiceModule.js
+++ leads/leadServiceModule.js
@@ -17,7 +17,7 @@ this.getLeads=function(){
    };
 	
 var leads=[
-{id:1, leadName:'Amit', responsiblePerson:'Gadaley', email:'agadaley@gmail.com', phoneNo:9584593069,companyType:'hotwax systems',industry:'engineer',annualIncome: 4000,currency: 'INR',comment: 'Comment just for testing purpose amit'},
+{id:1, leadName:'Amit', status:'In progress', sourceName:'...........',email:'gadaley@gmail.com', phoneNo:9584593069,company:'hotwax systems',addressLine1: 'vijaynagar',addressLine2: ''},
 {id:2, leadName:'Rahul', responsiblePerson:'Bhammarker', email:'rahul@gmail.com', phoneNo:111111111,companyType:'hotwax media',industry:'engineer',annualIncome: 4000,currency: 'USD',comment: 'Comment just for testing purpose by rahul'},
 {id:3, leadName:'Aman', responsiblePerson:'agrawal', email:'aman@gmail.com', phoneNo:999999999,companyType:'hotwax family',industry:'engineer',annualIncome: 4000,currency: 'USD',comment: 'Comment just for testing purpose by aman'},
 ];
