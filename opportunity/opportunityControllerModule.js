06de967dbb7c296bb6c7d089978413b83a47d17c
rahul <rahul@rahulbhalaptop.server.hwm>
2015-02-11 10:09:24 +0530

    F

--- opportunity/opportunityControllerModule.js
+++ opportunity/opportunityControllerModule.js
@@ -1,27 +1,25 @@
-var opportunityControllerModule=angular.module('crmApp.opportunityControllerModule',['crmApp.opportunityServiceModule']);
-opportunityControllerModule.controller('opportunityListController',function($scope,opportunityService)
-		{
-         init();
-         function init()
-         {
-        	 $scope.opportunities=opportunityService.getOpportunities();
-        	 $scope.hide=false;
-         };
-    }
-		
+var opportunityControllerModule = angular
+    .module('crmApp.opportunityControllerModule',
+        [ 'crmApp.opportunityServiceModule' ]);
+opportunityControllerModule.controller('opportunityListController', function(
+    $scope, opportunityService) {
+  init();
+  function init() {
+    $scope.opportunities = opportunityService.getOpportunities();
+    $scope.hide = false;
+  }
+  ;
+}
+
 );
 
-opportunityControllerModule.controller('viewOpportunityController', function($scope,$routeParams,opportunityService)
-		{
-	    var opportunityId;
-	    $scope.opportunity={};
-	    init();
-	    function init(){
-	         opportunityId=($routeParams.opportunityId);    
-	         $scope.opportunity=opportunityService.getOpportunity(opportunityId);   
-	        
-	    }
-	    
-
-	    
-	});
+opportunityControllerModule.controller('viewOpportunityController', function(
+    $scope, $routeParams, opportunityService) {
+  var opportunityId;
+  $scope.opportunity = {};
+  init();
+  function init() {
+    opportunityId = ($routeParams.opportunityId);
+    $scope.opportunity = opportunityService.getOpportunity(opportunityId);
+  }
+});
