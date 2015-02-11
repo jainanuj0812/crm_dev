ccda7c5243a579f46dab494eb47989bcea6e8d58
Aman29 <29aman92@gmail.com>
2015-02-11 10:13:05 +0530

    ...

--- leads/leadControllerModule.js
+++ leads/leadControllerModule.js
@@ -1,25 +1,25 @@
-var leadControllerModule=angular.module('crmApp.leadControllerModule',['crmApp.leadServiceModule']);
-leadControllerModule.controller('leadListController',function($scope,leadService)
-		{
-         init();
-         function init()
-         {
-        	 $scope.leads=leadService.getLeads();
-        	 $scope.hide=false;
-         }
-		}
-		
+var leadControllerModule = angular.module('crmApp.leadControllerModule',
+    [ 'crmApp.leadServiceModule' ]);
+leadControllerModule.controller('leadListController', function($scope,
+    leadService) {
+  init();
+  function init() {
+    $scope.leads = leadService.getLeads();
+    $scope.hide = false;
+  }
+}
+
 );
 
-leadControllerModule.controller('viewLeadController', function($scope,$routeParams,leadService){
-    var leadId;
-    $scope.lead={};
-    init();
-    function init(){
-         leadId=($routeParams.leadId);    
-         $scope.lead=leadService.getLead(leadId);   
-        
+leadControllerModule.controller('viewLeadController', function($scope,
+    $routeParams, leadService) {
+  var leadId;
+  $scope.lead = {};
+  init();
+  function init() {
+    leadId = ($routeParams.leadId);
+    $scope.lead = leadService.getLead(leadId);
+
   }
-     
-    
+
 });
--- leads/leadServiceModule.js
+++ leads/leadServiceModule.js
@@ -1,24 +1,60 @@
-var leadServiceModule=angular.module('crmApp.leadServiceModule',['crmApp.leadControllerModule']);
-leadServiceModule.service('leadService',function(){
+var leadServiceModule = angular.module('crmApp.leadServiceModule',
+    [ 'crmApp.leadControllerModule' ]);
+leadServiceModule.service('leadService', function() {
 
-this.getLeads=function(){
-		return leads;
-	};
+  this.getLeads = function() {
+    return leads;
+  };
 
-	this.getLead=function(id){
-        for(var i=0;i<leads.length-1;i++)
-        {
-            if(id==leads[i].id)
-                {return leads[i];}
-                
-        }
-        return null;
-            
-   };
-	
-var leads=[
-{id:1, leadName:'Amit Gadaley', status:'In progress', sourceName:'Cold call',email:'gadaley@gmail.com', phoneNo:9584593069,company:'hotwax systems',addressLine1: 'Scheme no 54',addressLine2: 'behind Sayaji Vijaya Nagar',country:'India',state:'M.P',description:'Testing purpose from amit'},
-{id:2, leadName:'Rahul bhammarker', status:'Assigned to', sourceName:'Conference',email:'bhammarker@gmail.com', phoneNo:9875232145,company:'Hotwax media',addressLine1: 'LIG-65',addressLine2: 'behind C21 Anand Nagar',country:'India',state:'u.p',description:'Testing purpose from rahul'},
-{id:3, leadName:'Saket Baghel', status:'Dead', sourceName:'Meeting',email:'saket@gmail.com', phoneNo:9425094250,company:'Hotwax Systems',addressLine1: 'Sneha Nagar',addressLine2: 'Behind Lotus Temple',country:'India',state:'New Delhi',description:'Test purpose from saket'},
-];
+  this.getLead = function(id) {
+    for (var i = 0; i < leads.length - 1; i++) {
+      if (id == leads[i].id) {
+        return leads[i];
+      }
+
+    }
+    return null;
+
+  };
+
+  var leads = [ {
+    id : 1,
+    leadName : 'Amit Gadaley',
+    status : 'In progress',
+    sourceName : 'Cold call',
+    email : 'gadaley@gmail.com',
+    phoneNo : 9584593069,
+    company : 'hotwax systems',
+    addressLine1 : 'Scheme no 54',
+    addressLine2 : 'behind Sayaji Vijaya Nagar',
+    country : 'India',
+    state : 'M.P',
+    description : 'Testing purpose from amit'
+  }, {
+    id : 2,
+    leadName : 'Rahul bhammarker',
+    status : 'Assigned to',
+    sourceName : 'Conference',
+    email : 'bhammarker@gmail.com',
+    phoneNo : 9875232145,
+    company : 'Hotwax media',
+    addressLine1 : 'LIG-65',
+    addressLine2 : 'behind C21 Anand Nagar',
+    country : 'India',
+    state : 'u.p',
+    description : 'Testing purpose from rahul'
+  }, {
+    id : 3,
+    leadName : 'Saket Baghel',
+    status : 'Dead',
+    sourceName : 'Meeting',
+    email : 'saket@gmail.com',
+    phoneNo : 9425094250,
+    company : 'Hotwax Systems',
+    addressLine1 : 'Sneha Nagar',
+    addressLine2 : 'Behind Lotus Temple',
+    country : 'India',
+    state : 'New Delhi',
+    description : 'Test purpose from saket'
+  }, ];
 });
